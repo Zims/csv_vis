fc3a36890bce4fa76f3c44b54b0ef8baa920ddb4
Zims <zims@inbox.lv>
2020-04-10 13:53:40 +0300

    get data

--- venv/main.py
+++ venv/main.py
@@ -1,14 +1,21 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 
-a = [1, 2, 3]
-b = [7, 8, 9]
-data = pd.read_csv(r'/Users/zims/PycharmProjects/csv_vis/venv/crime_rates.csv')
+data = pd.read_csv(r'crime_rates.csv', encoding='utf-8').fillna(0)
 # plt.plot(df.index['Year'], df.index['Burglary Rate'])
 print(data.columns)
-year_data = pd.DataFrame(data, columns= ['Year'])
-murder_rate_data = pd.DataFrame(data, columns= ['Murder Rate'])
+murder_data = data['Murder Rate'].iloc[0:1000].values
+population_data = data['Population'].iloc[0:1000].values
 
-# print(x, y)
-plt.plot(x, y)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_title("First ten murder rates")
+
+print(murder_data)
+ax.plot(murder_data, c='r')
+
+# plt.plot(year_data, murder_rate_data)
+# # plt.plot(x, y)
 plt.show()
+
+
