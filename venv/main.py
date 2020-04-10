a5be12b27dc0b32aaada7fa84c85c7a99840ada2
Zims <zims@inbox.lv>
2020-04-10 11:16:42 +0300

    initial

--- venv/main.py
+++ venv/main.py
@@ -6,8 +6,8 @@ b = [7, 8, 9]
 data = pd.read_csv(r'/Users/zims/PycharmProjects/csv_vis/venv/crime_rates.csv')
 # plt.plot(df.index['Year'], df.index['Burglary Rate'])
 print(data.columns)
-x = pd.DataFrame(data, columns= ['Year'])
-y = pd.DataFrame(data, columns= ['Murder Rate'])
+year_data = pd.DataFrame(data, columns= ['Year'])
+murder_rate_data = pd.DataFrame(data, columns= ['Murder Rate'])
 
 # print(x, y)
 plt.plot(x, y)
