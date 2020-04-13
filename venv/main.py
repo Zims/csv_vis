97c6b210618b884b6104ed0d71660312a70236d0
Zims <zims@inbox.lv>
2020-04-13 12:18:49 +0300

    template for everything

--- venv/main.py
+++ venv/main.py
@@ -3,6 +3,40 @@ from matplotlib import pyplot as plt
 
 
 sample_data = pd.read_csv(r'2014_apple_stock.csv')
-sample_data.head()
-plt.plot(sample_data.AAPL_x, sample_data.AAPL_y)
+data = pd.read_csv(r'countries.csv')
+cr_data = pd.read_csv(r'crime_rates.csv')
+print(cr_data.columns)
+# plt.plot(sample_data.AAPL_x, sample_data.AAPL_y)
+# plt.show()
+
+us = data[data.country == 'United States']
+china = data[data.country == 'China']
+
+florida = cr_data[cr_data.State == 'Florida']
+cali = cr_data[cr_data.State == 'California']
+
+plt.plot(florida.Year, florida.Population/10**6)
+plt.plot(cali.Year, cali.Population/10**6)
+
+plt.legend(['Florida', 'California'])
+plt.xlabel('Year')
+plt.ylabel('Population (millions)')
+
 plt.show()
+
+
+
+# plt.plot(us.year, us.population/ 10**6)
+# plt.plot(china.year, china.population/ 10**6)
+# plt.legend(['US', 'China'])
+# plt.xlabel('Year')
+# plt.ylabel('Population(millions)')
+
+# Percentage growth
+# us.population / us.population.iloc[0] * 100  #we get first year as 100% and so on
+# plt.plot(us.year, us.population / us.population.iloc[0] * 100)
+# plt.plot(china.year, china.population / china.population.iloc[0] * 100)
+# plt.legend(['US', 'China'])
+# plt.xlabel('Year')
+# plt.ylabel('Population(growth in %)')
+# plt.show()
